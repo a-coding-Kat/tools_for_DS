44bb0b97497112d59f4a7a937c6b7c6aba51ff09
Katarina Fatur <72339086+a-coding-Kat@users.noreply.github.com>
2024-02-22 23:40:50 +0100

    Create test.py
    add my for-loop

---
+++ test.py
@@ -0,0 +1,11 @@
+# Imagine you have a list of numbers, and you want to find every number higher than 10.
+
+num_list = [1, 5, 67, 34, 28, 3, 6, 90]
+
+larger_than_10 = []
+for num in num_list:
+  if num > 10:
+    larger_than_10.append(num)
+
+print(larger_than_10)
+    
